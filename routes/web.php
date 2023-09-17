503bbdebe281a2927529300a927af4c1a877dc80
philgill89 <phil.gillett@techquity.co.uk>
2023-09-17 13:05:11 +0100

    creating to do app

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TodoController::class, 'index']);
+
+Route::get('create', [TodoController::class, 'create']);
+
+Route::get('details/{todo}', [TodoController::class, 'details']);
+Route::get('edit/{todo}', [TodoController::class, 'edit']);
+Route::post('update/{todo}', [TodoController::class, 'update']);
+
+Route::get('delete/{todo}', [TodoController::class, 'delete']);
