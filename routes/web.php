94061963f7e59aad5b4554b30ca1ad2d02ee8730
philgill89 <phil.gillett@techquity.co.uk>
2023-09-20 22:58:12 +0100

    adds env

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::get('/', function(){
+    return view('index');
+});
 Route::get('/', [TodoController::class, 'index']);
-
 Route::get('create', [TodoController::class, 'create']);
-
 Route::get('details/{todo}', [TodoController::class, 'details']);
 Route::get('edit/{todo}', [TodoController::class, 'edit']);
 Route::post('update/{todo}', [TodoController::class, 'update']);
-
 Route::get('delete/{todo}', [TodoController::class, 'delete']);
 Route::post('store-data', [TodoController::class, 'store']);
